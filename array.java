7149f84ff9d48562367533d88efa3fcbe1dedb84
Bhavyaj2004 <143725856+Bhavyaj2004@users.noreply.github.com>
2024-03-07 16:42:51 +0530

    Create array.java

---
+++ array.java
@@ -0,0 +1,6 @@
+# Define an array
+  array = [64, 25, 12, 22, 11]
+#Sort the array using sorted() function
+  sort_array=sorted(array)
+# Display the sorted array
+  print("Sorted Array:", sort_array)
