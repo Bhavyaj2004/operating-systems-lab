8e51c9d78ca2d0f003cba3c3a4625f210d6942c6
Bhavyaj2004 <143725856+Bhavyaj2004@users.noreply.github.com>
2024-03-07 16:31:24 +0530

    Create arraydisplay.java

---
+++ arraydisplay.java
@@ -0,0 +1,14 @@
+Import java.util.Scanner;
+public class ArrayDisp {
+public static vold main(String[] args) {
+Int[] array.= new int[5]:
+Scanner sc= new Scanner (System.in);
+System.out.printin(""Original Array:");
+for (int i = 0; 1< array.length; I++) (
+System.out.printin("Enter Integer");
+array[i]= sc.nextint();
+} 
+for (int i = 0; i < array.length; i+)(
+System.out.printin(array[1);
+}
+}
