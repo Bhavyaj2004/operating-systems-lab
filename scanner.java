26737e02f8a7f5e92a279b985bd7be6ea74d53ba
Bhavyaj2004 <143725856+Bhavyaj2004@users.noreply.github.com>
2024-03-07 16:45:39 +0530

    Create scanner.java

---
+++ scanner.java
@@ -0,0 +1,55 @@
+// import java.util.Scanner;
+
+// public class ArrayDisp {
+//     public static void main(String[] args) {
+//         int[] array = new int[5];
+//         Scanner sc = new Scanner(System.in);
+//         for (int i = 0; i < array.length; i++) {
+//             System.out.println("Enter integer: ");
+//             array[i] = sc.nextInt(); // Corrected method name from nextlnt() to nextInt()
+//         }
+//         for (int i = 0; i < array.length; i++) {
+//             System.out.println(array[i]); // Corrected syntax from System.outprintln to System.out.println
+//         }
+//     }
+// }
+
+
+// import java.util.Scanner;
+
+// public class ArrayDisp {
+//     public static void main(String[] args) {
+//         String[] array = new String[5]; // Changed the type of array to String
+//         Scanner sc = new Scanner(System.in);
+//         for (int i = 0; i < array.length; i++) {
+//             System.out.println("Enter string: ");
+//             array[i] = sc.next(); // Reading strings using next() method
+//         }
+//         for (int i = 0; i < array.length; i++) {
+//             System.out.println(array[i]);
+//         }
+//     }
+// }
+
+// 
+
+// import java.util.Arrays;
+// import java.util.Scanner;
+
+// public class ArrayDisp {
+//     public static void main(String[] args) {
+//         String[] array = new String[5]; 
+//         Scanner sc = new Scanner(System.in);
+//         for (int i = 0; i < array.length; i++) {
+//             System.out.println("Enter string: "); 
+//             array[i] = sc.next(); 
+//         }
+        
+//         Arrays.sort(array);
+        
+//         System.out.println("Sorted Array:");
+//         for (int i = 0; i < array.length; i++) {
+//             System.out.println(array[i]); 
+//         }
+//     }
+// }
