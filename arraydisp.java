82d19b6d780a3c30213e7cf17b5e1173223098f4
Bhavyaj2004 <143725856+Bhavyaj2004@users.noreply.github.com>
2024-03-07 16:34:48 +0530

    Create arraydisp.java

---
+++ arraydisp.java
@@ -0,0 +1,16 @@
+Import java,util.arrays;
+public class ArrayDisp (
+public static void main(String[] args) (
+ 
+int[]a = new int[5]:
+Scanner sc= new Scanner (System.in);
+ 
+System.out.println/"OriginalArray:");
+for (inti= 0; [ < a,length; [++) {
+System.out.println("EnterInteger");
+a[l]=sc.nextint();
+}
+Arrays.sort(a);
+for (inti= 0; i< array.length; i++) (
+System.out.println(a[il);}
+}
